40343d9c7f042ef22db99ad0da1565d99ecef586
Choco1125 <sebascruz981125@gmail.com>
2024-05-30 15:48:59 -0500

    feat: creating app and hello world

--- inventory/urls.py
+++ inventory/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inventory_app import views 
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
