3ff542493dafac7405b34ededc3c7679f1d94bac
Choco1125 <sebascruz981125@gmail.com>
2024-05-30 16:00:33 -0500

    feat: creating routes on app

--- inventory_app/views.py
+++ inventory_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Hello world!</h1>')
+
+def hello(request, name):
+    return HttpResponse('<h1>Hello %s</h1>' % name)
